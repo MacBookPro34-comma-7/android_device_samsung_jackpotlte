81c7d61dcc3316f69f8c95e8d1ec3c5ea5eb926e
garubi04 <sleepone77@gmail.com>
2022-05-06 16:09:29 +0900

    Edited some files to make build possible.
    Change-Id: Ia63d07c6e842e583a92e5e7d9d2e47c6146620b5

--- lineage_jackpotlte.mk
+++ lineage_jackpotlte.mk
@@ -2,15 +2,26 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # Inherit full device configuration
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit from jackpotlte device
-$(call inherit-product, $(LOCAL_PATH)/device.mk)
+# Inherit device configuration
+$(call inherit-product, device/samsung/jackpotlte/device.mk)
 
-# Product ID
+# Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_jackpotlte
+PRODUCT_DEVICE := jackpotlte
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_MODEL := SM-A530N
+
+PRODUCT_CHARACTERISTICS := phone
+PRODUCT_SHIPPING_API_LEVEL := 29
+PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
+
