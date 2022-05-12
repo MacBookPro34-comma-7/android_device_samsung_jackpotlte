760769044bc838d9f719a4966675d632e9fcadd6
garubi04 <sleepone77@gmail.com>
2022-05-13 01:47:17 +0900

    Added a custom part for arm mail.
    Change-Id: I520e301b08308e81e93916a044fe0cf9040cad8a

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -32,5 +32,42 @@ write_headers
 
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
 
+###################################################################################################
+# CUSTOM PART START                                                                               #
+###################################################################################################
+OUTDIR=vendor/$VENDOR/$DEVICE
+(cat << EOF) >> $ANDROID_ROOT/$OUTDIR/Android.mk
+include \$(CLEAR_VARS)
+LOCAL_MODULE := libGLES_mali
+LOCAL_MODULE_OWNER := samsung
+LOCAL_SRC_FILES := proprietary/vendor/lib/egl/libGLES_mali.so
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_CHECK_ELF_FILES := false
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_PATH := \$(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/egl
+SYMLINKS := \$(TARGET_OUT)/vendor
+\$(SYMLINKS):
+	@echo "Symlink: vulkan.exynos5.so"
+	@mkdir -p \$@/lib/hw
+	\$(hide) ln -sf ../egl/libGLES_mali.so \$@/lib/hw/vulkan.exynos5.so
+	@echo "Symlink: libOpenCL.so"
+	\$(hide) ln -sf egl/libGLES_mali.so \$@/lib/libOpenCL.so
+	@echo "Symlink: libOpenCL.so.1"
+	\$(hide) ln -sf egl/libGLES_mali.so \$@/lib/libOpenCL.so.1
+	@echo "Symlink: libOpenCL.so.1.1"
+	\$(hide) ln -sf egl/libGLES_mali.so \$@/lib/libOpenCL.so.1.1
+ALL_MODULES.\$(LOCAL_MODULE).INSTALLED := \\
+	\$(ALL_MODULES.\$(LOCAL_MODULE).INSTALLED) \$(SYMLINKS)
+include \$(BUILD_PREBUILT)
+EOF
+(cat << EOF) >> $ANDROID_ROOT/$OUTDIR/$DEVICE-vendor.mk
+# Create Mali links for Vulkan and OpenCL
+PRODUCT_PACKAGES += libGLES_mali
+EOF
+###################################################################################################
+# CUSTOM PART END                                                                                 #
+###################################################################################################
+
 # Finish
 write_footers
