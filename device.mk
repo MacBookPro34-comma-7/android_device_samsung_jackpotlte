9a9654d12022324d58ba2cd56762a0e3a48cb9fb
hurrhnn <20sunrin022@sunrint.hs.kr>
2022-06-06 16:08:57 +0900

    jackpotlte: added apex, rendorscript configs.

--- device.mk
+++ device.mk
@@ -310,5 +310,8 @@ PRODUCT_PACKAGES += \
 # Properties
 include $(DEVICE_PATH)/system_prop.mk
 
+# Apex
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # call the proprietary setup
 $(call inherit-product, vendor/samsung/jackpotlte/jackpotlte-vendor.mk)
