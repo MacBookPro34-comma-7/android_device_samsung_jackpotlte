c528367de553604535ab29645d10a38ba32ac0cf
hurrhnn <20sunrin022@sunrint.hs.kr>
2022-06-09 06:29:08 +0900

    jackpotlte: correct ueventd.samsungexynos7885.rc location.

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -106,20 +106,10 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := ueventd.samsungexynos7885.rc
-LOCAL_MODULE_STEM  := ueventd.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/ueventd.samsungexynos7885.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := ueventd.rc
-LOCAL_MODULE_STEM  := ueventd.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/ueventd.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
