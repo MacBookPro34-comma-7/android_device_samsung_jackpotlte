2ae7ac9d2286c747dc3cbdf143aa6e456ce4de43
hurrhnn <20sunrin022@sunrint.hs.kr>
2022-06-08 23:31:55 +0900

    jackpotlte: added /efs additional path for sec_efs.

--- config.fs
+++ config.fs
@@ -1,10 +1,16 @@
+[efs]
+mode: 0771
+user: AID_SYSTEM
+group: AID_RADIO
+caps: 0
+
 [mnt/vendor/efs]
 mode: 0771
 user: AID_SYSTEM
 group: AID_RADIO
 caps: 0
 
-[mnt/vendor/cpefs/]
+[mnt/vendor/cpefs]
 mode: 0771
 user: AID_SYSTEM
 group: AID_RADIO
