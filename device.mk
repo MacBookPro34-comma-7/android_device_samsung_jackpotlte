93c309e459249f6f00e3df22acc0e2e48fb97b1b
hurrhnn <20sunrin022@sunrint.hs.kr>
2022-06-09 02:40:49 +0900

    jackpotlte: use 4GB dalvik-heap config.

--- device.mk
+++ device.mk
@@ -329,6 +329,9 @@ PRODUCT_PACKAGES += \
 # Properties
 include $(DEVICE_PATH)/system_prop.mk
 
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
+
 # Embedded SU
 ifeq ($(WITH_SU),true)
     SUPERUSER_EMBEDDED := true
