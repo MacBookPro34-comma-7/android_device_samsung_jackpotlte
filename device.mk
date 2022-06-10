3583ff8a9fb11b50937423cdadf744d89063af06
hurrhnn <20sunrin022@sunrint.hs.kr>
2022-06-11 03:25:01 +0900

    jackpotlte: added memtrack HAL services.

--- device.mk
+++ device.mk
@@ -299,7 +299,8 @@ PRODUCT_COPY_FILES += \
 
 # Memory
 PRODUCT_PACKAGES += \
-    android.hardware.memtrack@1.0-impl
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service
 
 # NFC
 PRODUCT_PACKAGES += \
