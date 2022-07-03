8709833169d26009e59a4db2000d976ae137b00f
hurrhnn <20sunrin022@sunrint.hs.kr>
2022-07-03 10:12:29 +0900

    jackpotlte: enable updating of apexes.

--- device.mk
+++ device.mk
@@ -379,6 +379,9 @@ include $(DEVICE_PATH)/system_prop.mk
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # call the proprietary setup
 $(call inherit-product, vendor/samsung/jackpotlte/jackpotlte-vendor.mk)
 
