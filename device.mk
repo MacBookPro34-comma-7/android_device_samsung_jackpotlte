733a1c0e1ae63e061bcaf07ecb26a3bc80c884f5
hurrhnn <20sunrin022@sunrint.hs.kr>
2022-07-21 04:38:56 +0900

    jackpotlte: uprev audio effect HAL to 6.0.
    also add an android.hardware.bluetooth.audio HAL.
    the audio HAL cannot uprev to 6.0 currently.
    
    Change-Id: Id1a98b304b1e638ec45e97ba72f0cc0a5ad2914b

--- device.mk
+++ device.mk
@@ -88,24 +88,23 @@ PRODUCT_PACKAGES += \
     libhidltransport \
     libhwbinder
 
+
+# Exclude AudioFX
+TARGET_EXCLUDES_AUDIOFX := true
+
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio.common-util \
-    android.hardware.audio.common@2.0-util \
-    android.hardware.audio.common@2.0 \
-    android.hardware.audio.common@4.0-util \
-    android.hardware.audio.common@4.0 \
-    android.hardware.audio.effect@2.0 \
-    android.hardware.audio.effect@4.0 \
-    android.hardware.audio@2.0 \
-    android.hardware.audio@4.0 \
-    android.hardware.soundtrigger@2.0-core \
-    android.hardware.soundtrigger@2.0 \
+    android.hardware.audio.service \
+    android.hardware.audio@6.0-impl:32 \
+    android.hardware.audio.effect@6.0-impl:32 \
+    android.hardware.audio.effect@6.0-service \
+    android.hardware.audio.common@6.0 \
+    android.hardware.audio.common@6.0-util \
+    android.hardware.bluetooth.audio@2.0 \
+    android.hardware.bluetooth.audio@2.0-impl \
     android.hardware.soundtrigger@2.1 \
-    android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio@2.0-impl \
-    android.hardware.soundtrigger@2.0-impl \
+    android.hardware.soundtrigger@2.1-impl \
+    android.hardware.soundtrigger@2.1-service \
     libalsautils \
     libeffects \
     libspeexresampler \
@@ -116,9 +115,22 @@ PRODUCT_PACKAGES += \
     audio.bluetooth.default \
     libtinycompress
 
+# Audio Legacy
+PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0 \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.common-util \
+    android.hardware.audio.common@2.0 \
+    android.hardware.audio.common@2.0-util
+
 PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/audio/mixer_gains.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_gains.xml \
+    $(DEVICE_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
     $(DEVICE_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
-    frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
+    frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
